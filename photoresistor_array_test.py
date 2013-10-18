6840d15b0b8c892ca81c7c33c84745a96e51ceef
Kevin McLaughlin <kmclaugh@bu.edu>
2013-10-18 15:19:16 -0400

    began work on the photoresistor test wrapper for this sepcific circuit

--- photoresistor_array_test.py
+++ photoresistor_array_test.py
@@ -3,191 +3,96 @@
 # parameters, running the netlist in LTSpice, and interpreting the output
 import re
 import os
+import itertools
+from python_ltspice_tools import *
 
-#----------------------------------------Find Param Equals----------------------------------------#{{{1
-def find_param_equals(parameter,line):
-    """finds the parameter and value for a given line and parameter"""
-    regex_string = "{} *= *\S+".format(parameter)
-    param_regex = re.compile(regex_string,re.IGNORECASE)
-    param_find = param_regex.findall(line)
-    if param_find != []:
-        param_find = param_find[0].split("=")
-        param_equals = param_find[1].replace(" ","")
-        return(param_equals)
-    else:
-        return(False)
-#----------------------------------------END Find Param Equals----------------------------------------#}}}
+def make_binary_string_values(binary_string,array_size,base_param_name):
+    """converts a binary string to an photorestor array values dictionary."""
+    x_total = array_size[0]
+    y_total = array_size[1]
+    array = []
+    for y in range(0,y_total):
+        row = []
+        for x in range(0,x_total):
+            param_name = base_param_name + str(x) + str(y)
+            row.append(param_name)
+        array.append(row)
+    photoresistor_array = {}
+    binary_count = 0
+    row_count = 0
+    while row_count <= y_total-1:
+        if row_count + 1 <= y_total-1:
+            for xy0,xy1 in zip(array[row_count],array[row_count+1]):
+                photoresistor_array[xy0] = int(binary_string[binary_count])
+                binary_count += 1
+                photoresistor_array[xy1] = int(binary_string[binary_count])
+                binary_count += 1
+        else:
+            for xy0 in array[row_count]:
+                photoresistor_array[xy0] = int(binary_string[binary_count])
+                binary_count += 1
+        row_count += 2
+    return(photoresistor_array)
 
-#----------------------------------------Original Line Class----------------------------------------#{{{1
-class original_line_class:
-    """container class for carrying around line and line numbers"""
-    def __init__(self,line,line_number):
-        self.line = line
-        self.number = line_number
-#----------------------------------------End Original Line Class----------------------------------------#}}}
-
-#----------------------------------------Process File----------------------------------------#{{{1
-def process_file(filename,parameter_header_name):
-    """Reads the .net file and picks out the parameters one is interested in"""
-    file = open(filename,"r")
-    original_file = []
-    parameter_lines = []
-    lines = file.readlines()
-    line_count = 0
-    param_line = False
-    for line in lines:
-        line=line[:-2]
-        original_line = original_line_class(line,line_count)
-        original_file.append(original_line)
-        if parameter_header_name in line:
-            if "End" in line:
-                end_param_statement = line
-                param_line = False
-                parameter_lines.append(original_line)
-            else:
-                start_param_statement = line
-                param_line = True
-        if param_line == True:
-            parameter_lines.append(original_line)
-        line_count += 1 
-    file.close()
-    return(original_file,parameter_lines)
-#----------------------------------------END Process File----------------------------------------#}}}
-
-#----------------------------------------Param Statement Class----------------------------------------#{{{1
-class param_statement_class:
-    """A class that defines a parameter in a netlist file. For instance:
-            R0=1k"""
-
-    def __init__(self,original_line,original_param_equals):
-        self.original_line = original_line
-        self.param_equals = original_param_equals
+def make_specific_test(array_values,base_param_name):
+    """convert an array of rows into a photoresistor array"""
+    x_total = len(array_values[0])
+    y_total = len(array_values)
+    array_names = []
+    for y in range(0,y_total):
+        row = []
+        for x in range(0,x_total):
+            param_name = base_param_name + str(x) + str(y)
+            row.append(param_name)
+        array_names.append(row)
+    photoresistor_array = {}
+    for row_name,row_value in zip(array_names,array_values):
+        for name,value in zip(row_name,row_value):
+            photoresistor_array[name]=value
+    return(photoresistor_array)
     
-    def replace_param_equals(self,new_param_equals,current_file_list):
-        current_line = current_file_list[self.original_line.number]
-        new_line = current_line.line.replace(self.param_equals,new_param_equals)
-        new_line = original_line_class(new_line,self.original_line.number)
-        current_file_list[self.original_line.number] = new_line
-        return(current_file_list)
-#----------------------------------------END Param Statement Clas----------------------------------------#}}}
+def make_array_values(Photoresistor_Array_Values):
+    """Turns a dictionary of photoresistors values into a list of params ready for simulation"""
+    pr_list = []
+    for key, value in Photoresistor_Array_Values.iteritems():
+        if value == 1:
+            value = "{Rligt}"
+        elif value == 0:
+            value = "{Rdark}"
+        this_pair = (key,value)
+        pr_list.append(this_pair)
+    return(pr_list)
 
-#----------------------------------------Switch Param Equals----------------------------------------#{{{1
-def switch_param_equals(change_params,original_file_list,parameter_lines):
-    """Switches parameter equals in the old file with the new given parameters.
-        change parameters are given as list in the format (parameter,new_equals)"""
-    current_file_list = original_file_list
-    for line in parameter_lines:
-        for change_param in change_params:
-            if change_param[0] in line.line: 
-                orig_param_equals = find_param_equals(change_param[0],line.line)
-                if orig_param_equals != False:
-                    param_statement = param_statement_class(line,orig_param_equals)
-                    param_statement.replace_param_equals(change_param[1],current_file_list)
-    return(current_file_list)
-#----------------------------------------END Switch Param Equals----------------------------------------#}}}
+def all_combinations(array_size):
+    """returns a list of binary strings that represent all possible combinations for the resistors.
+        given the size of the array as a tuple (X,Y)."""
+    total_resistors = array_size[0]*array_size[1]
+    all_combos = ["".join(seq) for seq in itertools.product("01", repeat=total_resistors)]
+    return(all_combos)
 
-#----------------------------------------Write New Net File----------------------------------------#{{{1
-def write_new_net_file(current_file_list,original_filename):
-    """Writes the new .net file so it is ready to run. Returns the new linux filename and wine filename"""
-    new_filename_linux = original_filename[:-4]+"_new"+".net"
-    file = open(new_filename_linux,"w")
-    for line in current_file_list:
-        file.write(line.line+"\n")
-    file.close()
-    new_filename_wine = new_filename_linux.split("LTspiceIV/")[-1]
-    return(new_filename_linux,new_filename_wine)
-#----------------------------------------END Write New Net File----------------------------------------#}}}
+def max_16_combinations(array_size):
+    """returns a list of binary string for a given array size of the first 16 possible combos.
+        This corresponds to a 4x4 array which is theoretically the maximum size where one photoresistor
+        could affect the other."""
+    total_resistors = array_size[0]*array_size[1]
+    all_combos = []
+    total_count = 0
+    for seq in itertools.product("01", repeat=total_resistors):
+        if total_count > 15:
+            break
+        this_value = "".join(seq)
+        all_combos.append(this_value)
+        total_count += 1
+    return(all_combos)
 
-#----------------------------------------Run Netlist----------------------------------------#{{{1
-def run_netlist(net_filename):
-    """Runs the given netlist assuming the netlist is in the LTspiveIV directory"""
-    command = "run_netlist.sh {}".format(net_filename)
-    os.system(command)
-#----------------------------------------END Run Netlist----------------------------------------#}}}
 
-#----------------------------------------Variable Value Class----------------------------------------#{{{1
-class variable_value_class:
-    """Container class for variable-value pairs"""
-    def __init__(self,variable,variable_number,variable_type,value=None,file_line=None):
-        self.variable = variable
-        self.value = value
-        self.variable_number = variable_number
-        self.variable_type = variable_type
-        self.find_unit()
-        self.file_line = file_line
-    def __repr__(self):
-        return_string = "{} = {} {}".format(self.variable,self.value,self.unit)
-        return(return_string)
-    def __str__(self):
-        return_string = "{} = {} {}".format(self.variable,self.value,self.unit)
-        return(return_string)
-    def find_unit(self):
-        """pulls the unit, ie V, A, Ohm, out of the variable type"""
-        if "voltage" in self.variable_type:
-            self.unit = "V"
-        elif "current" in self.variable_type:
-            self.unit = "A"
-#----------------------------------------END Variable Value Class----------------------------------------#}}}
 
-#----------------------------------------Pull Vairable----------------------------------------#{{{1
-def pull_variable(line):
-    """Pulls the variable, variable number, and variable type out of the line. Returns a vairable_value_class obj"""
-    elements = line.split("\t")
-    elements = elements[1:]
-
-    #variable number
-    variable_number = int(elements[0])
-    #variable name
-    variable = elements[1].split("(")
-    variable = variable[1].replace(")","")
-    #variable type
-    variable_type = elements[2]
-    variable_value_pair = variable_value_class(variable=variable,variable_number=variable_number,variable_type=variable_type)
-    return(variable_value_pair)
-#----------------------------------------END Pull Vairable----------------------------------------#}}}
-
-#----------------------------------------Pull Value----------------------------------------#{{{1
-def pull_value(line):
-    """pulls the float value out of a given value line and returns a float obj"""
-    float_regex = re.compile("-*\d.\d+e[-+]\d+")
-    string_value = float_regex.findall(line)[0]
-    float_value = float(string_value)
-    return(float_value)
-#----------------------------------------END Pull Value----------------------------------------#}}}
-
-#----------------------------------------Read Variables----------------------------------------#{{{1
-def read_variables(raw_filename):
-    "Read the raw file and collects vairables and values. Returns a list of variable_value_class objects"""
-
-    file = open(raw_filename,"r")
-    raw_file_lines = file.readlines()
-    file.close()
-    start_variables = False
-    start_values = False
-    line_count = 0
-    value_count = 0
-    variable_values = []
-    for line in raw_file_lines:
-        line=line[:-2]
-        if "Variables:" in line:
-            if "No. Variables" not in line:
-                start_variables = True
-
-        elif "Values:" in line:
-            start_variables = False
-            start_values = True
-
-        elif start_variables == True:
-            variable_value_pair = pull_variable(line)
-            variable_value_pair.file_line = line_count
-            variable_values.append(variable_value_pair)
-
-        elif start_values == True:
-            value = pull_value(line)
-            variable_values[value_count].value = value
-            value_count += 1
-    return(variable_values)
-#----------------------------------------END Read Variables----------------------------------------#}}}
+array_size = (2,2)
+combinations = max_16_combinations(array_size)
+quad_test = [[1,1],[1,0]]
+quad_test = make_specific_test(quad_test,"R")
+print(quad_test)
+Photoresistor_Array_Values = make_photoresistor_array_values(combinations[-1],array_size,"R")
 
 #process original file
 net_filename = "/home/kevin/.wine/drive_c/Program Files/LTC/LTspiceIV/Photoresistor_Array/Photoresistor_Array.net"
@@ -197,24 +102,17 @@ original_file_list = return_values[0]
 parameter_lines = return_values[1]
 
 #change parameters, create and run new file
-change_params = [("R01","5k"),("R11","10k")]
+change_params = make_array_values(Photoresistor_Array_Values)
 current_file_list = switch_param_equals(change_params,original_file_list,parameter_lines)
 new_net_filenames = write_new_net_file(current_file_list,original_filename=net_filename)
 new_net_filename_linux = new_net_filenames[0]
 new_net_filename_wine = new_net_filenames[1]
-run_netlist(new_net_filename_wine)
+#run_netlist(new_net_filename_wine)
 
 #read results
 raw_filename = new_net_filename_linux.replace(".net",".raw")
-print(raw_filename)
 variable_values =  read_variables(raw_filename)
-
-for variable_value in variable_values:
-    print(variable_value)
-        
-
-
-
-
+#for variable in variable_values:
+#    print(variable)
 
 
