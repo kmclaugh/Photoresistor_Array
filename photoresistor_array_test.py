d3ba92c2a0c3d4370c5904883e66acaf8af85d08
Kevin McLaughlin <kmclaugh@bu.edu>
2013-10-18 11:18:56 -0400

    finished array_test.py. Commiting before cleaning it up

--- photoresistor_array_test.py
+++ photoresistor_array_test.py
@@ -73,8 +73,14 @@ def switch_param_equals(change_params,original_file_list,parameter_lines):
                     param_statement.replace_param_equals(change_param[1],current_file_list)
     return(current_file_list)
 
+net_filename = "/home/kevin/.wine/drive_c/Program Files/LTC/LTspiceIV/Photoresistor_Array/Photoresistor_Array.net"
+parameter_header_name = "Resistance Parameters"
+return_values = process_file(net_filename,parameter_header_name)
+original_file_list = return_values[0]
+parameter_lines = return_values[1]
 
-def change_params_and_write_new_file(change_params,original_file_list=original_file_list,parameter_lines=parameter_lines,filename=filename):
+
+def change_params_and_write_new_file(change_params,original_file_list=original_file_list,parameter_lines=parameter_lines,filename=net_filename):
     """Writes the new .net file so it is ready to run"""
     new_filename = filename[:-4]+"_new"+".net"
     file = open(new_filename,"w")
@@ -92,13 +98,97 @@ def run_netlist(filename):
     command = "run_netlist.sh {}".format(filename)
     os.system(command)
 
-filename = "/home/kevin/.wine/drive_c/Program Files/LTC/LTspiceIV/Photoresistor_Array/Photoresistor_Array.net"
-parameter_header_name = "Resistance Parameters"
-return_values = process_file(filename,parameter_header_name)
-original_file_list = return_values[0]
-parameter_lines = return_values[1]
 
 change_params = [("R01","5k"),("R11","10k")]
 
-new_filename = change_params_and_write_new_file(change_params)
-run_netlist(new_filename)
+new_net_filename = change_params_and_write_new_file(change_params)
+#run_netlist(new_net_filename)
+
+class variable_value_class:
+    """Container class for varibale value pairs"""
+    def __init__(self,variable,variable_number,variable_type,value=None,file_line=None):
+        self.variable = variable
+        self.value = value
+        self.variable_number = variable_number
+        self.variable_type = variable_type
+        self.find_unit()
+        self.file_line = file_line
+    def __repr__(self):
+        return_string = "{} = {} {}".format(self.variable,self.value,self.unit)
+        return(return_string)
+    def __str__(self):
+        return_string = "{} = {} {}".format(self.variable,self.value,self.unit)
+        return(return_string)
+    def find_unit(self):
+        """pulls the unit, ie V, A, Ohm, out of the variable type"""
+        if "voltage" in self.variable_type:
+            self.unit = "V"
+        elif "current" in self.variable_type:
+            self.unit = "A"
+
+def pull_variable(line):
+    """Pulls the variable, variable number, and variable type out of the line. Returns a vairable_value_class obj"""
+    elements = line.split("\t")
+    elements = elements[1:]
+
+    #variable number
+    variable_number = int(elements[0])
+    #variable name
+    variable = elements[1].split("(")
+    variable = variable[1].replace(")","")
+    #variable type
+    variable_type = elements[2]
+    variable_value_pair = variable_value_class(variable=variable,variable_number=variable_number,variable_type=variable_type)
+    return(variable_value_pair)
+
+def pull_value(line):
+    """pulls the float value out of a given value line and returns a float obj"""
+    float_regex = re.compile("-*\d.\d+e[-+]\d+")
+    string_value = float_regex.findall(line)[0]
+    float_value = float(string_value)
+    return(float_value)
+
+def read_variables(raw_filename):
+    "Read the raw file and collects vairables and values. Returns a list of variable_value_class objects"""
+
+    file = open(raw_filename,"r")
+    raw_file_lines = file.readlines()
+    file.close()
+    start_variables = False
+    start_values = False
+    line_count = 0
+    value_count = 0
+    variable_values = []
+    for line in raw_file_lines:
+        line=line[:-2]
+        if "Variables:" in line:
+            if "No. Variables" not in line:
+                start_variables = True
+
+        elif "Values:" in line:
+            start_variables = False
+            start_values = True
+
+        elif start_variables == True:
+            variable_value_pair = pull_variable(line)
+            variable_value_pair.file_line = line_count
+            variable_values.append(variable_value_pair)
+
+        elif start_values == True:
+            value = pull_value(line)
+            variable_values[value_count].value = value
+            value_count += 1
+    return(variable_values)
+
+raw_filename = "/home/kevin/.wine/drive_c/Program Files/LTC/LTspiceIV/Photoresistor_Array/Photoresistor_Array.raw"
+variable_values =  read_variables(raw_filename)
+
+for variable_value in variable_values:
+    print(variable_value)
+        
+
+
+
+
+
+
