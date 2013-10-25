7e2086819855de320682aa48331eb059be2bca41
Kevin McLaughlin <kmclaugh@bu.edu>
2013-10-25 14:09:44 -0400

    Made 7X4 photoresistor array and ran small tests. It passed. Bug fixes to photoresistor test code. Commiting before running large test.

--- photoresistor_array_test.py
+++ photoresistor_array_test.py
@@ -5,124 +5,8 @@ import re
 import os
 import itertools
 import copy
+import datetime
 from python_ltspice_tools import *
-
-#---------------------------------------- Test Creators----------------------------------------#{{{1
-def make_photoresitor_array_values(Photoresistor_Array_Values):
-    """Turns a dictionary of photoresistors values into a list of params ready for simulation"""
-    pr_dict = {}
-    for key, value in Photoresistor_Array_Values.iteritems():
-        if value == 1:
-            value = "100"
-        elif value == 0:
-            value = "0"
-        else:
-            value = str(value)
-        pr_dict[key] = value
-    return(pr_dict)
-
-def make_binary_string_values(binary_string,array_size,base_param_name):
-    """converts a binary string to an photorestor array values dictionary."""
-    x_total = array_size[0]
-    y_total = array_size[1]
-    array = []
-    for y in range(0,y_total):
-        row = []
-        for x in range(0,x_total):
-            param_name = base_param_name + str(x)+ "_" + str(y)
-            row.append(param_name)
-        array.append(row)
-    photoresistor_array = {}
-    binary_count = 0
-    row_count = 0
-    while row_count <= y_total-1:
-        if row_count + 1 <= y_total-1:
-            for xy0,xy1 in zip(array[row_count],array[row_count+1]):
-                photoresistor_array[xy0] = int(binary_string[binary_count])
-                binary_count += 1
-                photoresistor_array[xy1] = int(binary_string[binary_count])
-                binary_count += 1
-        else:
-            for xy0 in array[row_count]:
-                photoresistor_array[xy0] = int(binary_string[binary_count])
-                binary_count += 1
-        row_count += 2
-    return(photoresistor_array)
-
-def make_specific_test(array_values,base_param_name):
-    """convert an array of rows into a photoresistor array"""
-    x_total = len(array_values[0])
-    y_total = len(array_values)
-    array_names = []
-    for y in range(0,y_total):
-        row = []
-        for x in range(0,x_total):
-            param_name = base_param_name + str(x) + "_" + str(y)
-            row.append(param_name)
-        array_names.append(row)
-    photoresistor_array = {}
-    for row_name,row_value in zip(array_names,array_values):
-        for name,value in zip(row_name,row_value):
-            photoresistor_array[name]=value
-    return(photoresistor_array)
-    
-def all_combinations(array_size):
-    """returns a list of binary strings that represent all possible combinations for the resistors.
-        given the size of the array as a tuple (X,Y)."""
-    total_resistors = array_size[0]*array_size[1]
-   
-    all_combos = ["".join(seq) for seq in itertools.product("01", repeat=total_resistors)]
-    return(all_combos)
-
-def max_16_combinations(array_size):
-    """returns a list of binary string for a given array size of the first 16 possible combos.
-        This corresponds to a 4x4 array which is theoretically the maximum size where one photoresistor
-        could affect the other."""
-    total_resistors = array_size[0]*array_size[1]
-    all_combos = []
-    total_count = 0
-    for seq in itertools.product("01", repeat=total_resistors):
-        if total_count > 15:
-            break
-        this_value = "".join(seq)
-        all_combos.append(this_value)
-        total_count += 1
-    return(all_combos)
-#----------------------------------------END Test Creators----------------------------------------#}}}
-
-#---------------------------------------- make voltage cycle array values----------------------------------------#{{{1
-def make_voltage_cycle_array_values(on_voltage,number_of_controls):
-    """Given the one control voltage integer that should be turns on, this returns the list of voltage
-        parameters with all but the given voltage turned off"""
-    v_param_list = {}
-    for v in range(0,number_of_controls):
-        vparam_name = "vcon" + str(v)
-        if v == on_voltage:
-            vvalue = 5
-        else:
-            vvalue = -5
-        v_param_list[vparam_name]=str(vvalue)
-    return(v_param_list)
-#----------------------------------------END make voltage cycle array value----------------------------------------#}}}
-
-#----------------------------------------One Cylce Photoresitor Array Test----------------------------------------#{{{1
-def one_cycle_photoresitor_test(photoresistor_array,array_size,netlist,base_test_name):
-    """Run a single cyle of voltages with the given photoresistor values for the given array size. 
-        Returns a list of the resulting the raw_values objects"""
-    results = []
-    netlist = netlist.change_parameters(variable_value_dictionary=photoresistor_array)
-    for v in range(0,array_size[1]):
-        this_test_name = base_test_name + "." + str(v)
-        v_param_list = make_voltage_cycle_array_values(v,array_size[1])
-        new_netlist = netlist.change_parameters(variable_value_dictionary=v_param_list)
-        print(this_test_name)
-        new_raw_values = new_netlist.run_netlist()
-
-        this_test_name = base_test_name + "." + str(v)
-        netlist_results = netlist_results_class(new_netlist,new_raw_values,test_name=this_test_name)
-        results.append(copy.deepcopy(netlist_results))
-    return(results)
-#----------------------------------------END One Cylce Photoresitor Array Test----------------------------------------#}}}
 
 #----------------------------------------Netlist-Results Class----------------------------------------#{{{1
 class netlist_results_class:
@@ -169,7 +53,7 @@ class netlist_results_class:
                     else:
                         self.passed = "Failed"
                         self.error_report.append("Error: Two Vcon were turned on")
-                vcontrols.append(parameter_statement_obj)
+                vcontrols.append((parameter_statement_obj,control_number))
             if "lux" in variable:
                 if "load" in variable:
                     pass
@@ -248,7 +132,11 @@ class netlist_results_class:
         """generates the string to be inserted into the report"""
         final_string = "Test {} {}\n".format(self.test_name,self.passed)
         resistor_string = self.resistance_parameter_string()
-        voltage_string = self.row_format_string % (self.vcontrols[0],self.vcontrols[2],self.vcontrols[1]) +"\n" #this is a bad hack
+        vcontrols = []
+        self.vcontrols = sorted(self.vcontrols, key=lambda x:x[1])
+        for vcon in self.vcontrols:
+            vcontrols.append(vcon[0])
+        voltage_string = self.row_format_string % tuple(vcontrols) +"\n" #this is a bad hack
         final_string += voltage_string
         final_string += resistor_string
         
@@ -265,6 +153,7 @@ class netlist_results_class:
 
 #----------------------------------------END Netlist-Results Class----------------------------------------#}}}                
 
+#----------------------------------------Write Report----------------------------------------#{{{1
 def write_report(results,report_filename,error_filename):
     """Write the report to a file in an intellegible manner"""
     report_file = open(report_filename,"w")
@@ -284,11 +173,128 @@ def write_report(results,report_filename,error_filename):
         error_file.write("No errors. Well done, sir")
     report_file.close()
     error_file.close()
-    
+#----------------------------------------END Write Report----------------------------------------#}}}
 
-def combinations_test(array_size):
+#----------------------------------------Make Voltage Cycle Array Values----------------------------------------#{{{1
+def make_voltage_cycle_array_values(on_voltage,number_of_controls):
+    """Given the one control voltage integer that should be turns on, this returns the list of voltage
+        parameters with all but the given voltage turned off"""
+    v_param_list = {}
+    for v in range(0,number_of_controls):
+        vparam_name = "vcon" + str(v)
+        if v == on_voltage:
+            vvalue = 5
+        else:
+            vvalue = -5
+        v_param_list[vparam_name]=str(vvalue)
+    return(v_param_list)
+#----------------------------------------END make voltage cycle array value----------------------------------------#}}}
+
+#----------------------------------------One Cylce Photoresitor Array Test----------------------------------------#{{{1
+def one_cycle_photoresitor_test(photoresistor_array,array_size,netlist,base_test_name):
+    """Run a single cyle of voltages with the given photoresistor values for the given array size. 
+        Returns a list of the resulting the raw_values objects"""
+    results = []
+    netlist = netlist.change_parameters(variable_value_dictionary=photoresistor_array)
+    for v in range(0,array_size[0]):
+        
+        this_test_name = base_test_name + "." + str(v)
+        v_param_list = make_voltage_cycle_array_values(v,array_size[0])
+        new_netlist = netlist.change_parameters(variable_value_dictionary=v_param_list)
+        
+        #Here is where the code actually runs a simulation
+        print(this_test_name)
+        new_raw_values = new_netlist.run_netlist()
+        netlist_results = netlist_results_class(new_netlist,new_raw_values,test_name=this_test_name)
+        results.append(copy.deepcopy(netlist_results))#needs to deep copy or the results class append will just be a pointer
+
+    return(results)
+#----------------------------------------END One Cylce Photoresitor Array Test----------------------------------------#}}}
+
+#---------------------------------------- Test Creators----------------------------------------#{{{1
+def make_photoresitor_array_values(Photoresistor_Array_Values):
+    """Turns a dictionary of photoresistors values into a list of params ready for simulation"""
+    pr_dict = {}
+    for key, value in Photoresistor_Array_Values.iteritems():
+        if value == 1:
+            value = "100"
+        elif value == 0:
+            value = "0"
+        else:
+            value = str(value)
+        pr_dict[key] = value
+    return(pr_dict)
+
+def make_binary_string_values(binary_string,array_size,base_param_name):
+    """converts a binary string to an photorestor array values dictionary."""
+    x_total = array_size[0]
+    y_total = array_size[1]
+    array = []
+    for y in range(0,y_total):
+        row = []
+        for x in range(0,x_total):
+            param_name = base_param_name + str(x)+ "_" + str(y)
+            row.append(param_name)
+        array.append(row)
+    photoresistor_array = {}
+    binary_count = 0
+    row_count = 0
+    while row_count <= y_total-1:
+        if row_count + 1 <= y_total-1:
+            for xy0,xy1 in zip(array[row_count],array[row_count+1]):
+                photoresistor_array[xy0] = int(binary_string[binary_count])
+                binary_count += 1
+                photoresistor_array[xy1] = int(binary_string[binary_count])
+                binary_count += 1
+        else:
+            for xy0 in array[row_count]:
+                photoresistor_array[xy0] = int(binary_string[binary_count])
+                binary_count += 1
+        row_count += 2
+    return(photoresistor_array)
+
+def make_specific_test(array_values,base_param_name):
+    """convert an array of rows into a photoresistor array"""
+    x_total = len(array_values[0])
+    y_total = len(array_values)
+    array_names = []
+    for y in range(0,y_total):
+        row = []
+        for x in range(0,x_total):
+            param_name = base_param_name + str(x) + "_" + str(y)
+            row.append(param_name)
+        array_names.append(row)
+    photoresistor_array = {}
+    for row_name,row_value in zip(array_names,array_values):
+        for name,value in zip(row_name,row_value):
+            photoresistor_array[name]=value
+    return(photoresistor_array)
+    
+def all_combinations(array_size):
+    """returns a list of binary strings that represent all possible combinations for the resistors.
+        given the size of the array as a tuple (X,Y)."""
+    total_resistors = array_size[0]*array_size[1]
+   
+    all_combos = ["".join(seq) for seq in itertools.product("01", repeat=total_resistors)]
+    return(all_combos)
+
+def max_combinations(array_size,max_number=16):
+    """Returns a binary string list of the first possible combinations up to the max number of tests
+        given"""
+    total_resistors = array_size[0]*array_size[1]
+    all_combos = []
+    total_count = 0
+    for seq in itertools.product("01", repeat=total_resistors):
+        if total_count > max_number-1:
+            break
+        this_value = "".join(seq)
+        all_combos.append(this_value)
+        total_count += 1
+    return(all_combos)
+
+def combinations_test(array_size,max_number=16):
     """Runs 16 different combinations of input since all would be too long"""
-    combinations = max_16_combinations(array_size)
+    combinations = max_combinations(array_size,max_number)
     all_results = []
     test_counter = 1
     for combination in combinations:
@@ -307,35 +313,54 @@ def combinations_test(array_size):
 def all_on_test(array_size):
     """Test with all the photoresistors turned on"""
     row_value = []
-    for y in range(0,array_size[0]):
-        row_value.append("1")
+    for x in range(0,array_size[0]):
+        row_value.append("100")
     array_values = []
-    for x in range(0,array_size[1]):
+    for y in range(0,array_size[1]):
         array_values.append(row_value)
     photoresistor_array = make_specific_test(array_values,"lux")
     all_results = one_cycle_photoresitor_test(photoresistor_array=photoresistor_array,array_size=array_size,netlist=original_netlist,
                                             base_test_name="all_on_1")
     return(all_results)
 
+def all_off_test(array_size):
+    """Test with all the photoresistors turned off"""
+    row_value = []
+    for y in range(0,array_size[0]):
+        row_value.append(0)
+    array_values = []
+    for x in range(0,array_size[1]):
+        array_values.append(row_value)
+    photoresistor_array = make_specific_test(array_values,"lux")
+    all_results = one_cycle_photoresitor_test(photoresistor_array=photoresistor_array,array_size=array_size,netlist=original_netlist,
+                                            base_test_name="all_off_1")
+    return(all_results)
+#----------------------------------------END Test Creators----------------------------------------#}}}
+
+#Test Parameters
 Vcon_on = "5"
 Vcon_off = "-5"
 Vread_on = 4.0 #V
 Vread_off = 0.1 #V
+max_combos_test = 16
 
+#Filenames
+original_filename = "/home/kevin/.wine/drive_c/Program Files/LTC/LTspiceIV/Photoresistor_Array/Photoresistor_Array.net"
+current_date = datetime.datetime.today()
+current_date_time_string = current_date.strftime("%m-%d-%Y-%H-%M")
+error_filename = "/home/kevin/Projects/Pill_Case/Design/Photoresistor_Array/Reports/error_report-{}.txt".format(current_date_time_string)
+report_filename = "/home/kevin/Projects/Pill_Case/Design/Photoresistor_Array/Reports/results_report-{}.txt".format(current_date_time_string)
 
 #process original file and get resistor and voltage parameters
-original_filename = "/home/kevin/.wine/drive_c/Program Files/LTC/LTspiceIV/Photoresistor_Array/Photoresistor_Array_Small.net"
 original_netlist = netlist_class(original_filename)
-array_size = (3,3)
+array_size = (7,4) #in (x,y) or (vcon,read)
 
+#Run Test Bench
+all_results = all_on_test(array_size)
+#all_results += all_off_test(array_size) all off is covered in combinations
+all_results += combinations_test(array_size,max_combos_test)
 
-
-error_filename = "/home/kevin/Projects/Pill_Case/Design/Photoresistor_Array/error_report.txt"
-report_filename = "/home/kevin/Projects/Pill_Case/Design/Photoresistor_Array/results_report.txt"
-
-all_on_results = all_on_test(array_size)
-combos_results = combinations_test(array_size)
-all_results = all_on_results + combos_results
+#Write Report Files
 write_report(all_results,report_filename,error_filename)
     
     
